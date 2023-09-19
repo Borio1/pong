f1b24ae3748866e059c3bad7c767cd9be6f60435
Borio1 <orlevi644@gmail.com>
2023-09-19 20:13:37 +0300

    pong 1.0

--- Proj/Form1.Designer.cs
+++ Proj/Form1.Designer.cs
@@ -33,15 +33,18 @@
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.timer2 = new System.Windows.Forms.Timer(this.components);
             this.ball = new System.Windows.Forms.PictureBox();
+            this.opponent = new System.Windows.Forms.PictureBox();
+            this.score = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.body)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.ball)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.opponent)).BeginInit();
             this.SuspendLayout();
             // 
             // body
             // 
             this.body.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
-            this.body.BackColor = System.Drawing.Color.Lime;
-            this.body.Location = new System.Drawing.Point(0, 399);
+            this.body.BackColor = System.Drawing.Color.White;
+            this.body.Location = new System.Drawing.Point(300, 399);
             this.body.Name = "body";
             this.body.Size = new System.Drawing.Size(200, 21);
             this.body.TabIndex = 0;
@@ -56,27 +59,50 @@
             // ball
             // 
             this.ball.BackColor = System.Drawing.Color.White;
-            this.ball.Location = new System.Drawing.Point(160, 380);
+            this.ball.Location = new System.Drawing.Point(390, 300);
             this.ball.Name = "ball";
             this.ball.Size = new System.Drawing.Size(20, 20);
             this.ball.TabIndex = 1;
             this.ball.TabStop = false;
             this.ball.Click += new System.EventHandler(this.ball_Click);
             // 
+            // opponent
+            // 
+            this.opponent.BackColor = System.Drawing.Color.White;
+            this.opponent.Location = new System.Drawing.Point(300, 41);
+            this.opponent.Name = "opponent";
+            this.opponent.Size = new System.Drawing.Size(200, 21);
+            this.opponent.TabIndex = 2;
+            this.opponent.TabStop = false;
+            // 
+            // score
+            // 
+            this.score.AutoSize = true;
+            this.score.ForeColor = System.Drawing.Color.Lime;
+            this.score.Location = new System.Drawing.Point(12, 9);
+            this.score.Name = "score";
+            this.score.Size = new System.Drawing.Size(41, 13);
+            this.score.TabIndex = 3;
+            this.score.Text = "Score: ";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Yellow;
+            this.BackColor = System.Drawing.Color.Black;
             this.ClientSize = new System.Drawing.Size(784, 461);
+            this.Controls.Add(this.score);
+            this.Controls.Add(this.opponent);
             this.Controls.Add(this.ball);
             this.Controls.Add(this.body);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = " ";
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
             ((System.ComponentModel.ISupportInitialize)(this.body)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.ball)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.opponent)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -86,6 +112,8 @@
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Timer timer2;
         private System.Windows.Forms.PictureBox ball;
+        private System.Windows.Forms.PictureBox opponent;
+        private System.Windows.Forms.Label score;
     }
 }
 
--- Proj/Form1.cs
+++ Proj/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
 
 namespace Proj
 {
@@ -16,10 +17,46 @@ namespace Proj
         {
             InitializeComponent();
         }
+        Random rnd = new Random();
 
-        int y = 1;
-        int x = 1;
+        public int y = -1;
+        public int x = 0;
+        public int W = 0;
+        public int L = 0;
+        Boolean start;
+        public int side = -1;
 
+        void win()
+        {
+            start = false;
+            x = 0;
+            y = 0;
+            W++;
+            side = 1;
+            ball.Location = new Point(Form1.ActiveForm.Width / 2 - 10, 140);
+            opponent.Location = new Point(Form1.ActiveForm.Width/2-100, 41);
+        }
+        void lose()
+        {
+            start = false;
+            x = 0;
+            y = 0;
+            L++;
+            side = -1;
+            ball.Location = new Point(Form1.ActiveForm.Width / 2 - 10, 300);
+            opponent.Location = new Point(Form1.ActiveForm.Width / 2 - 100, 41);
+        }
+        void opponentMove()
+        {
+            if (opponent.Location.X + 100 + rnd.Next(60) < ball.Location.X)
+            {
+                opponent.Left += 15;
+            }
+            if (opponent.Location.X + 100 + rnd.Next(60) > ball.Location.X && opponent.Location.X > 0)
+            {
+                opponent.Left -= 15;
+            }
+        }
         private void pictureBox1_Click(object sender, EventArgs e)
         {
 
@@ -27,25 +64,40 @@ namespace Proj
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            ballMove();
+            score.Text = "Score: " + W + "/" + L;
+            if (start)
+            {
+                ballMove();
+                opponentMove();
+                if (ball.Location.Y < -50)
+                {
+                    win();
+                }
+                if (ball.Location.Y > Form1.ActiveForm.Height)
+                {
+                    lose();
+                }
+            }
+            
         }
 
         void turn()
         {
-            if(ball.Bounds.IntersectsWith(body.Bounds))
+            if (ball.Bounds.IntersectsWith(body.Bounds))
             {
                 y = 1;
                 x = -2 * (ball.Location.X - body.Location.X - 100);
             }
-            if (ball.Location.Y <= 0)
+            if (ball.Bounds.IntersectsWith(opponent.Bounds))
             {
                 y = -1;
+                x = -2 * (ball.Location.X - opponent.Location.X - 100);
             }
             if (ball.Location.X <= 0)
             {
                 x = -Math.Abs(x);
             }
-            if (ball.Location.X+40 >= Form1.ActiveForm.Size.Width)
+            if (ball.Location.X + 45 >= Form1.ActiveForm.Size.Width)
             {
                 x = Math.Abs(x);
             }
@@ -55,22 +107,31 @@ namespace Proj
         {
             turn();
             ball.Top -= y * 20;
-            ball.Left -= x/10;
+            ball.Left -= x / 10;
         }
 
-        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        void Form1_KeyDown(object sender, KeyEventArgs e)
         {
-            if(e.KeyCode == Keys.Left)
+            if (e.KeyCode == Keys.Left && body.Location.X >= 1)
             {
                 body.Left -= 20;
             }
-            if (e.KeyCode == Keys.Right)
+            if (e.KeyCode == Keys.Right && body.Location.X <= Form1.ActiveForm.Size.Width-220)
             {
                 body.Left += 20;
             }
+            if (e.KeyCode == Keys.Space)
+            {
+                start = true;
+                y = side;
+            }
+        }
+        private void ball_Click(object sender, EventArgs e)
+        {
+      
         }
 
-        private void ball_Click(object sender, EventArgs e)
+        private void score_TextChanged(object sender, EventArgs e)
         {
 
         }
