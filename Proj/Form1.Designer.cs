a68d1ecf9afac3d67b10d258f5d4b60003696f3b
talmid <talmid@NWRYOSHKA1-13>
2023-09-19 14:13:32 +0300

    Add project files.

---
+++ Proj/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace Proj
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.body = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.ball = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.body)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // body
+            // 
+            this.body.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.body.BackColor = System.Drawing.Color.Lime;
+            this.body.Location = new System.Drawing.Point(0, 399);
+            this.body.Name = "body";
+            this.body.Size = new System.Drawing.Size(200, 21);
+            this.body.TabIndex = 0;
+            this.body.TabStop = false;
+            this.body.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // ball
+            // 
+            this.ball.BackColor = System.Drawing.Color.White;
+            this.ball.Location = new System.Drawing.Point(160, 380);
+            this.ball.Name = "ball";
+            this.ball.Size = new System.Drawing.Size(20, 20);
+            this.ball.TabIndex = 1;
+            this.ball.TabStop = false;
+            this.ball.Click += new System.EventHandler(this.ball_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Yellow;
+            this.ClientSize = new System.Drawing.Size(784, 461);
+            this.Controls.Add(this.ball);
+            this.Controls.Add(this.body);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.body)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox body;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.PictureBox ball;
+    }
+}
+
---
+++ Proj/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proj
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int y = 1;
+        int x = 1;
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            ballMove();
+        }
+
+        void turn()
+        {
+            if(ball.Bounds.IntersectsWith(body.Bounds))
+            {
+                y = 1;
+                x = -2 * (ball.Location.X - body.Location.X - 100);
+            }
+            if (ball.Location.Y <= 0)
+            {
+                y = -1;
+            }
+            if (ball.Location.X <= 0)
+            {
+                x = -Math.Abs(x);
+            }
+            if (ball.Location.X+40 >= Form1.ActiveForm.Size.Width)
+            {
+                x = Math.Abs(x);
+            }
+
+        }
+        void ballMove()
+        {
+            turn();
+            ball.Top -= y * 20;
+            ball.Left -= x/10;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Left)
+            {
+                body.Left -= 20;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                body.Left += 20;
+            }
+        }
+
+        private void ball_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
